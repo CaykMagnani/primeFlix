2e94c04f889dd7b57b3177479493155917001e2d
Cayk Magnani <cayk.andrade2004@gmail.com>
2023-10-17 20:08:58 -0300

    tESTE

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function RoutesApp() {
   return (
     <BrowserRouter>
-    <Header />
+        <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/Filme/:id' element={<Filme />} />
