1fcbbe2931b1235a78a1aea232a5263187c9deb0
Cayk Magnani <cayk.andrade2004@gmail.com>
2023-10-17 21:33:50 -0300

    Ta dando erro

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,25 @@
 import './style.css';
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-      <header>
-        <p>Aqui é o Header</p>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+  return (
+    <header>
+
+      <div className='logo'>
+        <img src='https://img.freepik.com/vetores-premium/filme-preto-logotipo-design-premium_39063-25.jpg' alt='logoEcommerce'/>
+      </div>
+
+      <Link className='linkSemSublinhado' to="/Filme/oi">
+
+        <button>
+          Meus filmes
+        </button>
+
+      </Link>
+
+    </header>
+    
+  )
+}
+
+export default Header;
